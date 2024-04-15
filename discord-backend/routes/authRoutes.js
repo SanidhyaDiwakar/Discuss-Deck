af97ee00255fc2e43161e128aac7c042a40f9d26
Sanidhya <sanidhyadiwakar@gmail.com>
2024-04-15 23:52:32 +0530

    Implemented Authentication Functionality

--- discord-backend/routes/authRoutes.js
+++ discord-backend/routes/authRoutes.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const authControllers = require("../controllers/authControllers");
+const auth = require("../middlewares/auth");
+const Joi = require("joi");
 
-router.post("/register", (req, res) => {
-  res.send("register route");
+const validator = require("express-joi-validation").createValidator({});
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(20).required(),
+  password: Joi.string().min(6).max(12).required(),
+  mail: Joi.string().email().required(),
 });
 
-router.post("/login", (req, res) => {
-  res.send("login route");
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).max(12).required(),
+  mail: Joi.string().email().required(),
 });
 
+router.post(
+  "/register",
+  validator.body(registerSchema),
+  authControllers.controllers.postRegister
+);
+
+router.post(
+  "/login",
+  validator.body(loginSchema),
+  authControllers.controllers.postLogin
+);
+
+//test route to verify if our middleware is wokring or not
+
+router.get("/test", auth, (req, res) => {
+  res.send("request passed");
+});
 module.exports = router;
